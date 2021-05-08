b35873b5a3c938d8fee26cdd0e84af23a7556eb5
Rohan Amin <rohana@armlab02.cs.princeton.edu>
2021-05-08 16:32:33 -0400

    dataA

--- createdataA.c
+++ createdataA.c
@@ -6,7 +6,8 @@
 int main(void) {
    FILE *psFile;
    int i;
-   unsigned long mov, adr, strb, branch; 
+   unsigned int mov, adr, strb, branch;
+   unsigned long movLocation; 
    psFile = fopen("dataA", "w");
    fputs("Rohan Alex",psFile);
 
@@ -17,11 +18,12 @@ int main(void) {
    adr = MiniAssembler_adr(1,0x420044,0x42007c) ;
    strb = MiniAssembeler_strb(0, 1); 
    branch = MiniAssembler_b(0x400864,0x420084);
-   
+   movLocation = 0x420078
    fwrite(&mov, sizeof(unsigned int), 1, psFile);
    fwrite(&adr, sizeof(unsigned int), 1, psFile);
    fwrite(&strb, sizeof(unsigned int), 1, psFile);
-   fwrite(&branch, sizeof(unsigned int), 1, psFile); 
+   fwrite(&branch, sizeof(unsigned int), 1, psFile);
+   fwrite (&movLocation, sizeof(unsigned long), 1, psFile); 
    fclose(psFile);
    return 0; 
 
